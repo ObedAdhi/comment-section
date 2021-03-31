ede0dcb0c3d1bbfe4333198e5ba6d38ef661efd3
ObedAdhi <obedadhinatapraya@yahoo.com>
2021-04-01 00:04:00 +0700

    changing axios base URL

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3000'
+  baseURL: 'https://simple-comment-section.herokuapp.com'
 })
 
 export default instance
